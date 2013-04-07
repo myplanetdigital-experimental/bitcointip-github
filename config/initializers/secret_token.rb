f8131c46e857fcf11b0cbfc48584285a6acee919
Patrick Connolly <patrick@myplanetdigital.com>
2013-04-07 17:59:31 -0400

    Initial rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BitcointipGithub::Application.config.secret_token = 'dcc3c04a9afe2416c1b5dd375240513c36a168f47031feab8ef552e402cf9e309c72ce787a6d2c3906e112455b4f176f0b865896b2075da51cf413f8c4eab255'

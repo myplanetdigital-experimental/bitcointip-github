53a8e6b21fe28fc465152b03b1c9b83df80f8e66
Patrick Connolly <patrick@myplanetdigital.com>
2013-04-07 23:49:47 -0400

    Adds basic auth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
   helper_method :current_user
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BITCOINTIP_BASICAUTH_USER'] && password == ENV['BITCOINTIP_BASICAUTH_PASS']
+    end
+  end
 
   private
 

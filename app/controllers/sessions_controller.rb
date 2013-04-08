dd0dfd29f1f779cc11f2caaf3416ccf9f7f9c0fe
Patrick Connolly <patrick@myplanetdigital.com>
2013-04-07 23:01:30 -0400

    Removed public.html. Adds session controller. Adds user model stub. Adds route for github callback.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,6 @@
+class SessionsController < ApplicationController
+  def create
+    auth = request.env['omniauth.auth']
+    user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) || User.create_with_omniauth(auth)
+  end
+end
